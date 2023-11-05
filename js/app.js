4a9026f14018d1fd9b5c136eb43f79652640e06a
Odd Egil Hegge Selnes <post@odde.org>
2023-11-05 17:50:49 +0100

    Fixed navigation
    Fixed an issue with mobile header

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ console.log('hei verden');
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navbarHeader = document.querySelector(".sticky-header");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navbarHeader.classList.toggle("active");
 });
